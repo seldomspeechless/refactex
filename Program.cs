e4b5fb443eac8273efb59d871d7b3df130d134bd
seldomspeechless <stranne@gmail.com>
2024-05-27 11:32:43 +0200

    Refactoring pre-lesson

--- Program.cs
+++ Program.cs
@@ -1,98 +1,86 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 
-namespace RefactoringExercise {
-    class Program {
-        static void Main(string[] args) {
-            DoubleStack stack = new DoubleStack();
-            while (true) {
-                if (stack.Depth == 0) {
-                    Console.WriteLine("Commands: q c + - * / number");
-                    Console.WriteLine("[]");
-                }
-                else {
-                    Console.WriteLine(stack.ToString());
-                }
-                string input = Console.ReadLine().Trim();
-                if (input == "") input = " ";
-                char command = input[0];
-                if (Char.IsDigit(command)) {
-                    double value = Convert.ToDouble(input);
-                    stack.Push(value);
-                }
-                else if (command == '+') {
+namespace RefactoringExercise;
+public class Program {
+    static void Main() {
+        var isRunning = true;
+        var stack = new DoubleStack();
+        while (isRunning) {
+            if (stack.Depth == 0) {
+                Console.WriteLine("Commands: q c + - * / number");
+                Console.WriteLine("[]");
+            }
+            else {
+                Console.WriteLine(stack.ToString());
+            }
+            var input = Console.ReadLine()?.Trim();
+            if (input is "" or null) input = " ";
+            var command = input[0];
+            switch (command) {
+                case '+': {
                     stack.Push(stack.Pop() + stack.Pop());
-                }
-                else if (command == '*') {
-                    stack.Push(stack.Pop() * stack.Pop());
-                }
-                else if (command == '-') {
-                    double d = stack.Pop();
-                    stack.Push(stack.Pop() - d);
-                }
-                else if (command == '/') {
-                    double d = stack.Pop();
-                    stack.Push(stack.Pop() / d);
-                }
-                else if (command == 'c') {
-                    stack.Clear();
-                }
-                else if (command == 'q') {
                     break;
                 }
-                else {
-                    Console.WriteLine("Illegal command, ignored");
+                case '*': {
+                    stack.Push(stack.Pop() * stack.Pop());
+                    break;
+                }
+                case '-': {
+                    var d = stack.Pop();
+                    stack.Push(stack.Pop() - d);
+                    break;
+                }
+                case '/': {
+                    var d = stack.Pop();
+                    stack.Push(stack.Pop() / d);
+                    break;
+                }
+                case 'c': {
+                    stack.Clear();
+                    break;
+                }
+                case 'q': {
+                    isRunning = false;
+                    break;
+                }
+                default: {
+                    if (char.IsDigit(command)) {
+                        var value = Convert.ToDouble(input);
+                        stack.Push(value);
+                    }
+                    else {
+                        Console.WriteLine("Illegal command, ignored");
+                    }
+                    break;
                 }
             }
         }
     }
+}
 
-    class DoubleStack {
-        private double[] data;
-        public int Depth { get; private set; }
+class DoubleStack {
+    private double[] _data = new double[1000];
+    public int Depth { get; private set; }
 
-        public DoubleStack() {
-            data = new double[1000];
-            Depth = 0;
+    public void Push(double value) => _data[Depth++] = value;
+    public double Pop() {
+        if (Depth > 0) {
+            return _data[--Depth];
         }
-
-        public void Push(double value) {
-            data[Depth++] = value;
-        }
-
-        public double Pop() {
-            if (Depth > 0) {
-                return data[--Depth];
-            }
-            else {
-                Console.WriteLine("stack empty, returning 0");
-                return 0;
-            }
-        }
-
-        public double Peek() {
-            if (Depth > 0) {
-                return data[Depth - 1];
-            }
-            else {
-                Console.WriteLine("stack empty, returning 0");
-                return 0;
-            }
-        }
-
-        public override string ToString() {
-            StringBuilder b = new StringBuilder();
-            b.Append('[');
-            for (int i = Depth - 1; ; i--) {
-                b.Append(data[i]);
-                if (i == 0)
-                    return b.Append(']').ToString();
-                b.Append(", ");
-            }
-        }
-
-        public void Clear() {
-            Depth = 0;
+        else {
+            Console.WriteLine("stack empty, returning 0");
+            return 0;
         }
     }
+    public override string ToString() {
+        var b = new StringBuilder();
+        b.Append('[');
+        for (int i = Depth - 1; ; i--) {
+            b.Append(_data[i]);
+            if (i == 0)
+                return b.Append(']').ToString();
+            b.Append(", ");
+        }
+    }
+    public void Clear() => Depth = 0;
 }
